e8a1c9fad9f436d32e35dcc2e1f6b3cf53679bc5
Pablo <psirera4@gmail.com>
2018-07-19 17:51:27 +0200

    Add tabulation html

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,23 +8,23 @@ import { NavController, IonicPage } from 'ionic-angular';
 @Component({
   selector: 'page-login',
   template: `
-  <ion-content padding>
-    <form (ngSubmit)="onSubmitForm()">
-      <ion-item>
-        <ion-label floating>Username</ion-label>
-        <ion-input type="text" name="username" [(ngModel)]="modelForm.user"></ion-input>
-      </ion-item>
+    <ion-content padding>
+      <form (ngSubmit)="onSubmitForm()">
+        <ion-item>
+          <ion-label floating>Username</ion-label>
+          <ion-input type="text" name="username" [(ngModel)]="modelForm.user"></ion-input>
+        </ion-item>
 
-      <ion-item>
-        <ion-label floating>Password</ion-label>
-        <ion-input type="password" name="password" [(ngModel)]="modelForm.pass"></ion-input>
-      </ion-item>
+        <ion-item>
+          <ion-label floating>Password</ion-label>
+          <ion-input type="password" name="password" [(ngModel)]="modelForm.pass"></ion-input>
+        </ion-item>
 
-      <div padding>
-        <button ion-button block>Sign In</button>
-      </div>
-    </form>
-  </ion-content>
+        <div padding>
+          <button ion-button block>Sign In</button>
+        </div>
+      </form>
+    </ion-content>
   `,
 })
 export class LoginPage {
