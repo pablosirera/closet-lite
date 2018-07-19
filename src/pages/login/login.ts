7bfa5f90645e4d08c7d0c7c8b1c8c131a1ea0015
Pablo <psirera4@gmail.com>
2018-07-19 17:26:36 +0200

    Remove html login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -7,7 +7,25 @@ import { NavController, IonicPage } from 'ionic-angular';
 
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  template: `
+  <ion-content padding>
+    <form (ngSubmit)="onSubmitForm()">
+      <ion-item>
+        <ion-label floating>Username</ion-label>
+        <ion-input type="text" name="username" [(ngModel)]="modelForm.user"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-label floating>Password</ion-label>
+        <ion-input type="password" name="password" [(ngModel)]="modelForm.pass"></ion-input>
+      </ion-item>
+
+      <div padding>
+        <button ion-button block>Sign In</button>
+      </div>
+    </form>
+  </ion-content>
+  `,
 })
 export class LoginPage {
   modelForm = {}
